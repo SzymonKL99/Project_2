af0a665778025528cb90a3afae84dbcec14ec775
Szymon Kliszcz <szymon.kliszczz@gmail.com>
2023-11-07 22:16:52 +0100

    extension

---
+++ src/main/java/com/crud/tasks/domain/AttachmentsByType.java
@@ -0,0 +1,7 @@
+package com.crud.tasks.domain;
+import lombok.Data;
+
+@Data
+public class AttachmentsByType {
+    private Trello trello;
+}
---
+++ src/main/java/com/crud/tasks/domain/Trello.java
@@ -0,0 +1,8 @@
+package com.crud.tasks.domain;
+import lombok.Data;
+
+@Data
+public class Trello {
+    private int board;
+    private int card;
+}
