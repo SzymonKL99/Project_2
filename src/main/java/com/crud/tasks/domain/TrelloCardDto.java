a3433e3a68909a691a1ecce3c6b658decd93c232
Szymon Kliszcz <szymon.kliszczz@gmail.com>
2023-11-09 16:59:32 +0100

    test

--- src/main/java/com/crud/tasks/domain/TrelloCardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloCardDto.java
@@ -1,11 +1,25 @@
 package com.crud.tasks.domain;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 public class TrelloCardDto {
 
+    @JsonProperty("name")
     private String name;
+
+    @JsonProperty("description")
     private String description;
+
+    @JsonProperty("pos")
     private String pos;
+
+    @JsonProperty("listId")
     private String listId;
 }
